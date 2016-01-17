3ce4a9d54b5d4b1427537d6e84ae46247db6529b
Murphy Berzish <murphy.berzish@gmail.com>
2016-01-17 00:30:34 -0500

    add transport tube world updates

--- src/model/world_updates.cc
+++ src/model/world_updates.cc
@@ -1,5 +1,56 @@
 #include "world_updates.h"
 
+static TransportTube *find_transport(World *w, Vector position, uint32_t transportID) {
+    auto occupants = w->get_occupants(position);
+    for (VoxelOccupant *occ : occupants) {
+        if (!(occ->is_transport_tube())) continue;
+        TransportTube *transport = (TransportTube*)occ;
+        if (transport->get_transport_id() == transportID) {
+            return transport;
+        }
+    }
+    return NULL;
+}
+
+WorldUpdateResult ConnectTransportEndpointUpdate::apply(World *w) {
+    // TODO ConnectTransportEndpointUpdate::apply()
+    return new WorldUpdateResult(false);
+}
+
+WorldUpdateResult ConnectTransportTubeUpdate::apply(World *w) {
+    // TODO make sure the two positions are adjacent
+    // look for transport tubes with this transportID in first/second positions
+    TransportTube *first = find_transport(w, position1, transportID);
+    TransportTube *second = find_transport(w, position2, transportID);
+    if (first == NULL || second == NULL) {
+      return new WorldUpdateResult(false);
+    }
+    // make sure that <2 connections per transport
+    if (first->get_number_of_connected_devices() > 1 || second->get_number_of_connected_devices() > 1) {
+      return new WorldUpdateResult(false);
+    }
+    // connect each to the other
+    first->connect(second);
+    second->connect(first);
+    return new WorldUpdateResult(true);
+}
+
+WorldUpdateResult CreateTransportTubeUpdate::apply(World *w) {
+    TransportTube *transport = new TransportTube(transportID);
+    if (w->can_occupy(position, transport)) {
+        bool result = w->add_occupant(position, Vector(0,0,0), transport);
+        if (result) {
+            return WorldUpdateResult(true);
+        } else {
+            delete transport;
+            return WorldUpdateResult(false);
+        }
+    } else {
+        delete transport;
+        return WorldUpdateResult(false);
+    }
+}
+
 WorldUpdateResult RemoveObjectUpdate::apply(World *w) {
 	w->remove_occupant(target);
 	return WorldUpdateResult(true);
--- src/model/world_updates.h
+++ src/model/world_updates.h
@@ -3,19 +3,43 @@
 
 #include "world.h"
 #include "voxel_occupant.h"
+#include "transport_endpoint.h"
+#include "transport_tube.h"
 
 // TODO complete world updates header
 
 class ConnectTransportEndpointUpdate : public WorldUpdate {
-
+public:
+    ConnectTransportEndpointUpdate(uint32_t txID, Vector pos, TransportEndpoint *ept, uint32_t eptID)
+        : transportID(txID), position(pos), endpoint(ept), endpointID(eptID) {}
+    ~ConnectTransportEndpointUpdate(){}
+    WorldUpdateResult apply(World *w);
+protected:
+    uint32_t transportID;
+    Vector position;
+    TransportEndpoint *endpoint;
+    uint32_t endpointID;
 };
 
 class ConnectTransportTubeUpdate : public WorldUpdate {
-
+public:
+    ConnectTransportTubeUpdate(uint32_t txID, Vector pos1, Vector pos2) : transportID(txID), position1(pos1), position2(pos2) {}
+    ~ConnectTransportTubeUpdate() {}
+    WorldUpdateResult apply(World *w);
+protected:
+    uint32_t transportID;
+    Vector position1;
+    Vector position2;
 };
 
 class CreateTransportTubeUpdate : public WorldUpdate {
-
+public:
+    CreateTransportTubeUpdate(Vector position, uint32_t txID) : position(position), transportID(txID) {}
+    ~CreateTransportTubeUpdate() {}
+    WorldUpdateResult apply(World *w);
+protected:
+    Vector position;
+    uint32_t transportID;
 };
 
 class MiningLaserUpdate : public WorldUpdate {
