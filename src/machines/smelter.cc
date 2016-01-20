6f93f986b899c6e5886482cc07bb624e5f792205
Murphy Berzish <murphy.berzish@gmail.com>
2016-01-20 16:14:29 -0500

    smelter implementation, requires lots of stuff from items though

---
+++ src/machines/smelter.cc
@@ -0,0 +1,78 @@
+#include "smelter.h"
+
+Smelter::Smelter()
+: state(STATE_LOAD), currentOre(NULL), smeltingTimeLeft(0) {
+
+}
+
+Smelter::~Smelter() {
+	if (currentOre != NULL) {
+		delete currentOre;
+	}
+	for (Item *i : outputQueue) {
+		delete i;
+	}
+}
+
+bool Smelter::receive_to_endpoint(uint32_t eptID, Item *item) {
+	if (eptID != 0) {
+		return false;
+	}
+	if (state != STATE_LOAD) {
+		return false;
+	}
+	// TODO we can only receive Ore
+	Ore ore = (Ore*)item;
+	// the ore has to be something we can smelt
+	if (ore->get_material()->can_be_smelted()) {
+		return false;
+	}
+	// okay, receive input and start smelting
+	currentOre = ore;
+	state = STATE_SMELT;
+	smeltingTimeLeft = ore->get_material->get_smelting_timesteps();
+	return true;
+}
+
+void Smelter::pre_send_timestep() {
+	switch (state) {
+	case STATE_LOAD:
+		// don't do anything
+		break;
+	case STATE_SMELT:
+	{
+		// smelt one step
+		smeltingTimeLeft -= 1;
+		if (smeltingTimeLeft == 0) {
+			// produce bars and queue them for output
+			for (uint32_t i = 0; i < ore->get_material()->get_number_of_smelted_bars(); ++i) {
+				outputQueue.add(ComponentLibrary::inst()->create_component("bar", currentOre->get_material()));
+			}
+			delete currentOre;
+			currentOre = NULL;
+			state = STATE_OUTPUT;
+		}
+	} break;
+	case STATE_OUTPUT:
+	{
+		if (outputQueue.empty()) {
+			state = STATE_LOAD;
+		} else {
+			set_endpoint_output(1, outputQueue.front());
+		}
+	} break;
+	} // switch(state)
+}
+
+virtual void Smelter::post_send_timestep(std::unordered_map<uint32_t,bool> results) {
+	for (auto entry : results) {
+		bool status = entry.second;
+		if (status) {
+			outputQueue.pop();
+			// if the queue became empty because of this, we can load again
+			if (outputQueue.empty()) {
+				state = STATE_LOAD;
+			}
+		}
+	}
+}
---
+++ src/machines/smelter.h
@@ -0,0 +1,49 @@
+#ifndef _MACHINES_SMELTER_
+#define _MACHINES_SMELTER_
+
+#include <cstdint>
+#include "transport_endpoint.h"
+#include <queue>
+
+/*
+ * A Smelter is a machine that refines smeltable ore into bars of material
+ * that can then be used to produce other items.
+ *
+ * Endpoints: 0=input, 1=output
+ */
+
+class Smelter : public TransportEndpoint {
+public:
+	Smelter();
+	virtual ~Smelter();
+
+	virtual std::unordered_set<uint32_t> get_transport_endpoints() const {
+		return std::unordered_set<uint32_t>{0,1};
+	}
+	virtual Vector get_extents() const {
+		return Vector(1,1,1);
+	}
+	virtual bool has_world_updates() const {
+		return false;
+	}
+	virtual bool receive_to_endpoint(uint32_t eptID, Item *item);
+	virtual uint32_t get_type() const { return 1; }
+
+	Ore* get_current_ore() const { return currentOre;}
+	uint32_t get_smelting_time_remaining() const { return smeltingTimeLeft; }
+protected:
+	enum SmelterState {
+		STATE_LOAD, // waiting to receive ore
+		STATE_SMELT, // smelting ore
+		STATE_OUTPUT // waiting to output bars
+	};
+	SmelterState state;
+	Ore *currentOre;
+	uint32_t smeltingTimeLeft;
+	std::queue<Item*> outputQueue;
+
+	virtual void pre_send_timestep();
+	virtual void post_send_timestep(std::unordered_map<uint32_t,bool> results);
+};
+
+#endif // _MACHINES_SMELTER_
